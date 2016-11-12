6a0119d863460b726f2ae7f9fda29a88973e8d91
mchogan7 <mills.c.hogan@gmail.com>
2016-11-12 16:52:35 -0600

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var HanSolo = {Name: 'President Solo',
+                Atk: 6,
+                CnAtk: 12,
+                HP: 120,
+                PlayerChar: false};
+
+
+HanSolo.PlayerChar = true;
+console.log(HanSolo.PlayerChar)
