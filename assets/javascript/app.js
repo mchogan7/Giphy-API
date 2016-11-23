6e7fa73d59cc602e423181ac0e71cf1f85d3d673
mchogan7 <mills.c.hogan@gmail.com>
2016-11-22 20:35:54 -0600

    Added abilty for a user created quiz.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,31 @@
 function game() {
-var set = 0
-    //Timer object
+    var set = 0
+        //Timer object.
     var timer = {
         time: 30,
-        tick:  function() {
+        tick: function() {
+            if (timer.time === 0) {
+                timer.stop()
+            }
 
-                if (timer.time === 0) {
-                    timer.stop()
-                }
-
-                if (timer.time > 9) {
-                    $('.timer').html(timer.time)
-                } else {
-                    $('.timer').html('0' + timer.time)
-                }
-                timeCheck();
-                timer.time -= 1
-                console.log('tick')
-
-                },
+            if (timer.time > 9) {
+                $('.timer').html(timer.time)
+            } else {
+                $('.timer').html('0' + timer.time)
+            }
+            timeCheck();
+            timer.time -= 1
+        },
+        //These funcitons control start stop & reset of the timer.
         reset: function() {
             timer.time = 30;
-             $('.timer').html(timer.time)
+            $('.timer').html(timer.time)
         },
         stop: function() {
             clearInterval(set)
         },
-        start: function(){
-            console.log('start')
-           set = setInterval(timer.tick, 1000)
+        start: function() {
+            set = setInterval(timer.tick, 1000)
         }
 
     };
@@ -43,13 +40,16 @@ var set = 0
     //Increments to move to next set of questions
     var increment = 0
 
-    //List of questions in this object
+    userArray = false;
+
+
+
+    //List of questions
 
     var questions = [{
             question: "Do you like Dinosaurs?",
             answer: "Yes",
-            wrongAnswer: ["No"
-            ]
+            wrongAnswer: ["No"]
         },
 
         {
@@ -70,7 +70,7 @@ var set = 0
             ]
         },
 
-           {
+        {
             question: "A T-Rex boarded a train that traveled at an average speed of 100 kilometers per hour, stopping for 3 minutes after every 75 kilometers. A Dilophosaurus boarded a train that traveled at an average speed of 50 kilometers, stopping for 1 minute after every 25 kilometers. If the trains began traveling at the same time, how many kilometers did the Dilophosaurus train travel in the time it took the T-Rex train to travel 600 kilometers? ",
             answer: "307.5 km",
             wrongAnswer: ["300 km",
@@ -81,7 +81,7 @@ var set = 0
             ]
         },
 
-             {
+        {
             question: "What is the Family scientific classification of Albertadromeus?",
             answer: "Thescelosauridae",
             wrongAnswer: ["Orodrominae",
@@ -94,7 +94,7 @@ var set = 0
             ]
         },
 
-               {
+        {
             question: "Which of the following appears at the 129th place of the DNA sequence of a Stegosaurus?",
             answer: "TTT",
             wrongAnswer: ["GCT",
@@ -117,15 +117,44 @@ var set = 0
         },
 
     ]
-
+    
+    var clone = questions.slice(0);
+    console.log(clone);
+    //Begins the game and renders initial HTML.
     pushToStart()
 
 
+    //This function sets off the game. It calls all other functions based on clicks.
+        function pushToStart() {
+        $('.toStart').html("<div class ='title'>SUPER DINO QUIZ</div><div class='button start'>Press to Start.</div><div class='button userQuiz'>Make your own quiz.</div>")
+        if(userArray === true){
+             $('.toStart').append("<div class='button takeUserQuiz'>Retake your quiz.</div>")
+            $('.takeUserQuiz').click(function() {
+            timer.stop();
+            timer.reset();
+            $('.toStart').html("")
+            $('.triviaArea').html("");
+            displayQuestions()
+            timerIn();
+        })
 
-    //Push to Start
+        }
+        $('.start').click(function() {
+            questions = clone.slice(0);
+            timer.stop();
+            timer.reset();
+            $('.toStart').html("")
+            $('.triviaArea').html("");
+            displayQuestions()
+            timerIn();
+            userArray = false
+        })
+            $('.userQuiz').click(function() { makeUserForm()
+        questions = [] });
+    }
 
-    //Display questions with answers placed in random order
-
+   
+    //This function randomizes the order of correct and wrong answers in an array.
     function randomizer() {
         randomArray = [];
         randomArray.push(questions[increment].answer)
@@ -137,6 +166,7 @@ var set = 0
         });
     }
 
+    //This function Displays the question and the randomized answers.
     function displayQuestions() {
         timer.start()
         randomizer();
@@ -145,29 +175,18 @@ var set = 0
             .addClass("question")
             .text(questions[increment].question)
         );
-          $('.triviaArea').append($('<ol>'))
+        $('.triviaArea').append($('<ol>'))
 
         for (var i = 0; i < randomArray.length; i++) {
             $('ol').append('<li><span class ="answer">' + randomArray[i] + '</span></li>');
-                    }
+        }
         answerCheck();
     }
 
-    function pushToStart() {
-        $('.toStart').html("Press to Start.")
-        $('.toStart').click(function() {
-            timer.stop();
-            timer.reset();
-            $(this).html("")
-            $('.triviaArea').html("");
-            displayQuestions()
-            timerIn();
-        })
-    }
-
+    //This function checks for the correct answer on click.
     function answerCheck() {
         $('.answer').click(function() {
-             timerOut()
+            timerOut()
             if ($(this).html() === questions[increment].answer) {
                 winScreen();
                 timer.stop()
@@ -178,6 +197,7 @@ var set = 0
         })
     }
 
+    //On a correct guess, this screen is displayed. It continues to the next question after a delay.
     function winScreen() {
         timer.stop();
         $('.toStart').html("Good job! " + questions[increment].answer + " is correct!")
@@ -185,7 +205,7 @@ var set = 0
         correct += 1;
         setTimeout(nextQuestion, 3000);
     }
-
+    //Same as above but for loses.
     function loseScreen() {
         timer.stop();
         $('.toStart').html("<div class='red'>Sorry! " + questions[increment].answer + " is the correct answer.</div>")
@@ -193,30 +213,25 @@ var set = 0
         wrong += 1;
         setTimeout(nextQuestion, 3000);
     }
-
+    //Moves the game to the next question until all questions in the array are used.
     function nextQuestion() {
-        if(increment === questions.length - 1){
+        if (increment === questions.length - 1) {
             finalScore();
-            console.log('over')
+        } else {
+            increment += 1
+            $('.toStart').html("")
+            randomArray = []
+            timer.reset()
+            timerIn();
+            displayQuestions()
         }
-        else{
-        increment += 1
-        $('.toStart').html("")
-        randomArray = []
-        timer.reset()
-        timerIn();
-        console.log("nextQ")
-        displayQuestions()
-    }
-      
-
     }
 
+    //Keeps track of the remaining time. Display a lose screen when it reaches 0.
     function timeCheck() {
         timerShake();
         if (timer.time === 0) {
             timerOut();
-            console.log('times up!')
             $('.toStart').html("<div class='red'>Time is up! " + questions[increment].answer + " is the correct answer.</div>")
             $('.triviaArea').html("");
             outOfTime += 1;
@@ -224,22 +239,22 @@ var set = 0
         }
     }
 
-    function finalScore(){
-            $('.triviaArea').html("<div class='final'>Correct Answers: " + correct + "</div><div class='final red'>Wrong Answers: " + wrong + "</div><div class='final red'>Ran Out of Time: " + outOfTime + "</div>"
-                );
-                                
-    $('.toStart').html("")
-    $('.triviaArea').append(
+    //This renders the final score and gives the option reset the game and play again.
+    function finalScore() {
+        $('.triviaArea').html("<div class='final'>Correct Answers: " + correct + "</div><div class='final red'>Wrong Answers: " + wrong + "</div><div class='final red'>Ran Out of Time: " + outOfTime + "</div>");
+        $('.toStart').html("")
+        $('.triviaArea').append(
             $('<div/>')
             .addClass("button")
             .addClass("reset")
             .text("Play again.")
-            )
-    $('.reset').click(function(){resetGame()});
+        )
+        $('.reset').click(function() { resetGame() });
+        }
 
-    }
 
-    function resetGame(){
+        //Returns the game to initial settings.
+    function resetGame() {
         $('.triviaArea').html("");
         $('.toStart').html("");
         correct = 0;
@@ -247,43 +262,80 @@ var set = 0
         outOfTime = 0;
         increment = 0;
         randomArray = [];
-        console.log(randomArray)
         pushToStart();
     }
 
-    function timerIn(){
+    //Controls the animation of the timer moving down into the window.
+    function timerIn() {
         $('.timer').removeClass('redTimer')
         $('.timer').removeClass('timerDown')
         $('.timer').addClass('timerUp')
     }
 
-      function timerOut(){
+    //Controls animation of moving timer up out of the window.
+    function timerOut() {
         $('.timer').removeClass('timerUp')
         $('.timer').addClass('timerDown')
     }
 
-    function timerShake(){
-        if(timer.time <= 10 && timer.time > 0){
-           $('.timer').addClass('redTimer') 
-        $('.timer').effect( "shake", {times: 4, distance: 5}, 250 );
+    //Activates a red shaking animation when the timer reaches 10 seconds.
+    function timerShake() {
+        if (timer.time <= 10 && timer.time > 0) {
+            $('.timer').addClass('redTimer')
+            $('.timer').effect("shake", { times: 4, distance: 5 }, 250);
+        }
     }
+
+
+    //This builds the user quiz section
+    $('.userQuiz').click(function() { makeUserForm()
+        questions = [] });
+
+    function makeUserForm() {
+        $('.toStart').html("<div class ='title'>Make your own quiz:</div>" +
+            "<div class ='formHeader'>Enter Question</div>" +
+            "<form><input class ='userQuestion' type='text'></form>" +
+            "<div class ='formHeader'>Enter Answer</div>" +
+            "<form><input class = 'userAnswer' type='text'></form>" +
+            "<div class ='formHeader'>Enter Wrong Answers</div>" +
+            "<form><input class = 'userWrong' type='text'></form>")
+        $('.triviaArea').html(
+            "<div class ='addMore'>+ Add more wrong answers.</div>" +
+            "<div class ='next button'>Next Question.</div>" +
+            "<div class ='submit button'>Submit and Take Quiz.</div>")
+        $('.addMore').click(function() {
+            $('.toStart').append("<form><input class = 'userWrong' type='text'></form>")
+        })
+
+        //User can move to create next question
+        $('.next').click(function() {
+            saveQuestion();
+            makeUserForm();
+        })
+        //User submits their questions and starts quiz.
+        $('.submit').click(function() {
+            saveQuestion();
+            $('.triviaArea').html("")
+            $('.toStart').html("")
+            timerIn();
+            displayQuestions()
+        });
+        userArray = true;
+    }
+    //Pushes user question object to the main object array. It then follows the same path as the default questions.
+    function saveQuestion() {
+        var userObject = {
+            question: $('.userQuestion').val(),
+            answer: $('.userAnswer').val(),
+            wrongAnswer: []
+        }
+        $(".userWrong").each(function() {
+            userObject.wrongAnswer.push($(this).val())
+        });
+        questions.push(userObject)
+        console.log(clone);
     }
 
-
-
-
-
-
-
-    //Setup win/lose parameters based on user choice or time running out
-
-    //Show correct answer. Message depends on win, lose or out of time
-
-    //Repeat for the rest of questions.
-
-    //Tally all correct, wrong and out of time answers and display.
-
-    //Ask to play again and reset.
 
 }
-game()
+game() //the initial call of the entire game function.
